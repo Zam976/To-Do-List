3931a1f6b6942f9beb78b9259b13b89935940573
moumini <zamzammoumini@gmail.com>
2024-05-07 17:45:16 +0200

    trosième

--- index.php
+++ index.php
@@ -19,26 +19,39 @@ require 'db_conn.php';
                 placeholder="This field is required"/>
                 <button type="submit"> Add &nbsp; <span>&#43;</span></button>
             </form>
-            </div>
+        </div>
             <?php
               $todos = $conn->query('SELECT * FROM todos ORDER BY id DESC');
             ?>
             <div class="show-todo-section">
-                <?php if($todos->rowCount() === 0){ ?>
+                <?php if($todos->rowCount() <= 0){ ?>
                     <div class="todo-item">
             <div class="empty">
-                <img src="" >
+                <img src="img/modified.png" width = 100% >
+                <img src="img/delecte.png" width = 100% >
             </div>
             </div> 
         <?php } ?>
-                <div class="todo-item">
-                <input type="checkbox">
-                <h2> Voici la liste</h2>
+        <?php while ($todo = $todos->fetch(PDO::FETCH_ASSOC)) { ?>
+             <div class="todo-item">
+                <span id = "<?php echo $todo['id'];?>"
+                class="remove-to-do">x</span>
+            <?php if ($todo['checked']){ ?>
+                   <input type="checkbox"
+                   class="check-box"
+                   checked />
+                <h2 class ="checked"> <?php echo $todo['title'] ?></h2>
+             <?php } else {?>
+                <input type="checkbox"
+                   class="check-box" />
+                <h2> <?php echo $todo['title'] ?></h2>
+            <?php } ?>
                 <br>
-                <small>created:05/05/224</small>
+                <small>created:05/05/24</small>
             
-        </div>
-        </div>
+              </div>
+         <?php } ?>
+        </div> 
     </div>
 </body>
 </html>
